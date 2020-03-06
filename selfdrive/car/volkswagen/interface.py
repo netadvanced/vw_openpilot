3e3a2fd07d3632a1ade238a249af3c89dd521342
Jason Young <jyoung8607@gmail.com>
2020-03-05 19:41:01 -0500

    Remove cp_cam valid check for no LKAS cars

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -119,7 +119,7 @@ class CarInterface(CarInterfaceBase):
     self.pt_cp.update_strings(can_strings)
     self.cam_cp.update_strings(can_strings)
     self.CS.update(self.pt_cp, self.cam_cp)
-    ret.canValid = self.pt_cp.can_valid and self.cam_cp.can_valid
+    ret.canValid = self.pt_cp.can_valid
 
     # Wheel and vehicle speed, yaw rate
     ret.wheelSpeeds.fl = self.CS.wheelSpeedFL
